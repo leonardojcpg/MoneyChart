942f0adc117be6793e0c6d5b017c075d09a50239
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-06-16 21:50:50 -0300

    feat: Creating ChartStats component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/header";
+import { ChartStats } from "./components/stats"
 import "./global.css"
 
 function App() {
   return (
     <div>
       <Header/>
+      <ChartStats />
     </div>
   );
 }
--- src/components/stats/index.jsx
+++ src/components/stats/index.jsx
@@ -1,10 +1,12 @@
-export const Chart = () => {
+import "./styles.css"
+
+export const ChartStats = () => {
   return (
-    <>
+    <div className="chart-container">
       <div className="mainTitle">
         <h3>Spending - Last 7 days</h3>
       </div>
       <div className="chart"></div>
-    </>
+    </div>
   );
 };
